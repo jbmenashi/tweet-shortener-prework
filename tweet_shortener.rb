ee63f290898d747adcb7d27ac2eef60218090595
Jacob Menashi <jbmenashi@gmail.com>
2018-10-05 17:17:56 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,3 +34,20 @@ def bulk_tweet_shortener(tweets)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweets)
+  tweets.each do |tweet|
+   if tweet.length > 140
+     word_substituter(tweet)
+   else
+     tweet
+   end
+ end
+end
+ 
+     
+     
+     
+     
+     
+     
